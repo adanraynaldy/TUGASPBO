5ef1f873fc0ba1b523f9276a97b7e2e5927b3d60
adanraynaldy <105645032+adanraynaldy@users.noreply.github.com>
2022-05-16 19:14:37 +0700

    A11.2017.10774 - ADAN RAYNALDY
    PERTEMUAN 1

---
+++ Latihan1/src/week1/Latihan2b.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class Latihan2b {
+    public static void main(String[] args) {
+        int a=6, t=8;
+        double luas;
+        
+        luas=0.5*a*t;
+        System.out.println("Luas Segitiga : "+luas);
+    }  
+}
---
+++ Latihan1/src/week1/Latihan2c.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class Latihan2c {        
+
+	public static void main(String[] args) {
+             double hasil1, hasil2, d=5, t=10;
+             double phi = 3.14;
+             
+        hasil2 = d / 2;
+        hasil1 = (phi * hasil2 * hasil2 * t);
+       
+        System.out.println("Volume Tabung : " + hasil1);
+    }
+}
